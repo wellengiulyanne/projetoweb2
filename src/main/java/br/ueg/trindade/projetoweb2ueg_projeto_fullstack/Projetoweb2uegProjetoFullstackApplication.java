791b0f84dc19365c66a66eff5368b195b1d65175
wellengiulyanne <wellensousa570@gmail.com>
2026-08-21 21:21:31 -0300

    Projeto web2

---
+++ src/main/java/br/ueg/trindade/projetoweb2ueg_projeto_fullstack/Projetoweb2uegProjetoFullstackApplication.java
@@ -0,0 +1,13 @@
+package br.ueg.trindade.projetoweb2ueg_projeto_fullstack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetoweb2uegProjetoFullstackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetoweb2uegProjetoFullstackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ueg/trindade/projetoweb2ueg_projeto_fullstack/Projetoweb2uegProjetoFullstackApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ueg.trindade.projetoweb2ueg_projeto_fullstack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Projetoweb2uegProjetoFullstackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
